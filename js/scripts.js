3c9c4f4345817cde86c488acb09182615947fe46
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-01-17 02:53:44 +0300

    Try to get data from sizeRadio inputs

--- js/scripts.js
+++ js/scripts.js
@@ -20,6 +20,8 @@ $(document).ready(function() {
     $("#menu-item-" + i).click(function() {
       $("#pizza-type").text(pizzaList[i - 1].name);
       $(".modal-img").attr("src", "assets/img/" + pizzaList[i - 1].image);
+      let pizzaSize = $(".sizeRadio:checked").val();
+      console.log(pizzaSize);
    });
   }
   
