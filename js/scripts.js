2b7a2384ae49155bb9a88bb9469bc76238de3914
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-01-17 00:56:26 +0300

    Add pizza types

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//business logic
+function PizzaType(name, image) {
+  this.name = name;
+  this.image = image;
+}
+
+const pizzaList = [
+  new PizzaType("Chicken Mushroom", "pizza-chicken-mushroom.jpg"),
+  new PizzaType("Pepperoni", "pizza-pepperoni.jpg"),
+  new PizzaType("Hawaiian", "pizza-hawaiian.jpg"),
+  new PizzaType("Margarita", "pizza-margarita.jpg"),
+  new PizzaType("Salami", "pizza-salami.jpg"),
+  new PizzaType("Seafood", "pizza-seafood.jpg")
+];
+
+
+//user-interface logic
