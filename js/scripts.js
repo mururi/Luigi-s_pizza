13106ed66ecbc3d8c698f357a5341fdaccf7de21
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-01-17 15:35:55 +0300

    Add toppings and crusts lists

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,16 @@ function PizzaType(name, image) {
   this.image = image;
 }
 
+function Crust(name, price) {
+  this.name = name;
+  this.price = price;
+}
+
+function Topping(name, price) {
+  this.name = name;
+  this.price = price;
+}
+
 const pizzaList = [
   new PizzaType("Chicken Mushroom", "pizza-chicken-mushroom.jpg"),
   new PizzaType("Pepperoni", "pizza-pepperoni.jpg"),
@@ -13,6 +23,27 @@ const pizzaList = [
   new PizzaType("Seafood", "pizza-seafood.jpg")
 ];
 
+const crustList = [
+  new Crust("Regular", "0"),
+  new Crust("Crispy", "100"),
+  new Crust("Stuffed", "150"),
+  new Crust("Gluten-free", "200")
+];
+
+const toppingsList = [
+  new Topping("Cheddar", "200"),
+  new Topping("Feta","200"),
+  new Topping("Green chilli","150"),
+  new Topping("Green pepper","150"),
+  new Topping("Jalapeno","150"),
+  new Topping("Mushroom","150"),
+  new Topping("Olives","150"),
+  new Topping("Onion","150"),
+  new Topping("Pineapple","150"),
+  new Topping("Salami","150"),
+  new Topping("Tikka sauce","100"),
+  new Topping("Peri-peri sauce","100"),
+];
 
 //user-interface logic
 $(document).ready(function() {
@@ -20,8 +51,7 @@ $(document).ready(function() {
     $("#menu-item-" + i).click(function() {
       $("#pizza-type").text(pizzaList[i - 1].name);
       $(".modal-img").attr("src", "assets/img/" + pizzaList[i - 1].image);
-      let pizzaSize = $(".sizeRadio:checked").val();
-      console.log(pizzaSize);
+      let selectedType = 
    });
   }
   
